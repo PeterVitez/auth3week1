58cc4ea19a402aa21c733ac94efcd280abd83c1f
PeterVitez <pvitez6@gmail.com>
2020-10-08 14:17:39 -0400

    added template for prof data and iterrated it

--- js/main.js
+++ js/main.js
@@ -1,14 +1,23 @@
 // import your packages here
-import Team from './modules/DataModule.js'
-;(() => {
+import Team from "./modules/DataModule.js";
+(() => {
   // stub * just a place for non-component-specific stuff
-  console.log('loaded')
 
-  console.log(Team)
+  let userSection = document.querySelector(".users-section"),
+    userTemplate = document.querySelector("#profs-template").content;
 
-  let userSection = document.querySelector('.users-section').children
+  function handleDataSet(data) {
+    for (let user in data) {
+      let currentUser = userTemplate.cloneNode(true),
+        currentUserText = currentUser.querySelector(".user").children;
 
-  userSection[1].textContent = Team.Justin.name
-  userSection[2].textContent = Team.Justin.role
-  userSection[3].textContent = Team.Justin.nickname
-})()
+      currentUserText[1].textContent = data[user].name;
+      currentUserText[2].textContent = data[user].role;
+      currentUserText[3].textContent = data[user].nickname;
+
+      userSection.appendChild(currentUser);
+    }
+  }
+
+  handleDataSet(Team);
+})();
--- js/modules/DaTaModule.js
+++ js/modules/DaTaModule.js
@@ -1,22 +1,27 @@
 export default {
   Justin: {
-    name: 'Justin',
-    role: 'Coordinator',
-    nickname: 'THE COORDINATOR',
+    name: "Justin",
+    role: "Coordinator",
+    nickname: "THE COORDINATOR",
   },
   John: {
-    name: 'John',
-    role: 'super chill prof',
-    nickname: 'just John',
+    name: "John",
+    role: "super chill prof",
+    nickname: "just John",
   },
   Joe: {
-    name: 'Joe',
-    role: 'Prof',
-    nickname: 'Teddy Bear',
+    name: "Joe",
+    role: "Prof",
+    nickname: "Teddy Bear",
   },
   Pan: {
-    name: 'Pan',
-    role: 'Prof',
-    nickname: 'SpiderPan',
+    name: "Pan",
+    role: "Prof",
+    nickname: "SpiderPan",
   },
-}
+  Jarrod: {
+    name: "Jarrod",
+    role: "Male Model Prof",
+    nickname: "Zoolander",
+  },
+};
