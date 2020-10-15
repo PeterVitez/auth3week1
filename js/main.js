a462cd9c88bd4c054f2701b893be70e15e4829c4
PeterVitez <pvitez6@gmail.com>
2020-10-15 13:07:17 -0400

    add class files -broken-

--- js/main.js
+++ js/main.js
@@ -1,7 +1,37 @@
 // import your packages here
-import Team from "./modules/DataModule.js";
+// import Team from "./modules/DataModule.js";
 (() => {
   // stub * just a place for non-component-specific stuff
+
+  //set up the XMLHttp object
+  let myReq = new XMLHttpRequest();
+
+  // make sure we can handle whaterver data comes back, or any errors
+  myReq.addEventListener("readystatechange", handleRequest);
+
+  //open a request and pass through the URL of the data that we want
+  myReq.open("GET", "../DataSet.json");
+
+  //actually making the request
+  myReq.send();
+
+  //handleRequest function goes here
+  function handleRequest() {
+    if (myReq.readyState === XMLHttpRequest.DONE) {
+      // check status here and proceed
+      if (myReq.status === 200) {
+        // 200 means done and dusted, ready to go with the dataset!
+        handleDataSet(myReq.responseText);
+      } else {
+        // probably got some kind of error code, so handle that
+        // a 404, 500 etc... can render appropriate error messages here
+        console.error(`${myReq.status} : something done broke, son`);
+      }
+    } else {
+      // request isn't ready yet, keep waiting...
+      console.log(`Request state: ${myReq.readyState}. Still processing...`);
+    }
+  }
 
   let userSection = document.querySelector(".users-section"),
     userTemplate = document.querySelector("#profs-template").content;
